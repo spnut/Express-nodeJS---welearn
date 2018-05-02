43dda4e303869c7db2e37b7903f778f5d2cf5963
admin <nutty1997@live.com>
2018-05-03 02:49:12 +0700

    add about page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 var tutorRouter = require('./routes/tutor');
+var aboutRouter = require('./routes/about')
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 app.use('/tutor', tutorRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
